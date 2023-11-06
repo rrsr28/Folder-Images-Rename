4338c5861639be13c15cabdd1abf5272eaa89ec6
rrsr28 <rr.sanjayram@gmail.com>
2023-11-06 19:57:24 +0530

    Choose your own directoy feature

--- ImagesRenamer.py
+++ ImagesRenamer.py
@@ -1,15 +1,21 @@
 import os
 import glob
+import tkinter as tk
+from tkinter import filedialog
 
-directory_path = 'your_directory_path'
+# Create a root Tk window and hide it
+root = tk.Tk()
+root.withdraw()
+
+directory_path = filedialog.askdirectory()
 
 image_files = glob.glob(os.path.join(directory_path, '*.jpg')) + glob.glob(os.path.join(directory_path, '*.jpeg')) + glob.glob(os.path.join(directory_path, '*.png')) + glob.glob(os.path.join(directory_path, '*.gif'))
-image_files.sort()
+
 
 for i, image_file in enumerate(image_files, start=1):
-  
+
   base = os.path.basename(image_file)
   name, ext = os.path.splitext(base)
 
-  new_image_file = os.path.join(directory_path, 'Screenshot' + str(i) + ext)
+  new_image_file = os.path.join(directory_path, 'Screenshot ' + str(i) + ext)
   os.rename(image_file, new_image_file)
