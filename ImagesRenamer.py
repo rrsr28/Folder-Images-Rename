58ca6ecb9579672bf3f376fa50c567097632b6f0
rrsr28 <rr.sanjayram@gmail.com>
2023-11-06 19:54:17 +0530

    Created the file to rename all the screenshots in the screenshot folder

---
+++ ImagesRenamer.py
@@ -0,0 +1,15 @@
+import os
+import glob
+
+directory_path = 'your_directory_path'
+
+image_files = glob.glob(os.path.join(directory_path, '*.jpg')) + glob.glob(os.path.join(directory_path, '*.jpeg')) + glob.glob(os.path.join(directory_path, '*.png')) + glob.glob(os.path.join(directory_path, '*.gif'))
+image_files.sort()
+
+for i, image_file in enumerate(image_files, start=1):
+  
+  base = os.path.basename(image_file)
+  name, ext = os.path.splitext(base)
+
+  new_image_file = os.path.join(directory_path, 'Screenshot' + str(i) + ext)
+  os.rename(image_file, new_image_file)
