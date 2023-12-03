258e6cac71d4ff9825edfd558474ee72f5726893
rrsr28 <rr.sanjayram@gmail.com>
2023-12-03 17:39:39 +0530

    Minor change to fileName while saving

--- ImagesRenamer.py
+++ ImagesRenamer.py
@@ -12,5 +12,5 @@ for i, image_file in enumerate(image_files, start=1):
   base = os.path.basename(image_file)
   name, ext = os.path.splitext(base)
 
-  new_image_file = os.path.join(directory_path, 'Screenshot ' + str(i) + ext)
+  new_image_file = os.path.join(directory_path, 'Screenshot (' + str(i) + ext + ')')
   os.rename(image_file, new_image_file)
